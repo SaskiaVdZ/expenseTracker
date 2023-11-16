11a5101a6861ea71b89a731217b748f7ba7cee78
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2023-11-16 16:53:26 +0100

    cleaned up and added comments

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-// why parseFloat? because it turns/stores the prompt input back as a decimal number
-// why did I use if else statements and not switch?  If else seems to be the easiest way.
+
 
 // OBJECT
 
-// regular function declaration
-function myFunc() {
-  // kod
-}
-
-// before you had the function keyword first
 const accountObject = {
   firstName: "Saskia",
   expensesArray: [],
-  // empty expenses array
+  // empty expense array
   incomeArray: [],
   // empty income array
 
@@ -20,56 +13,37 @@ const accountObject = {
     // this is a function declaration as a property, switch the keyword function the name
     //addIncome.push(2)
 
-    //we want to add something from the prompt
     //const expens = parseFloat(prompt("Please fill in your expens here"));
     const expenseDescription = prompt("Please describe your type of expense, for example food, petrol, rent, clothes:");
-   
+  
     const expenseAmount = parseFloat(prompt(`Please add the amount you spend on ${expenseDescription}:`));
-
+   
+    // making sure the user only fills in numbers not strings
     if (isNaN(expenseAmount)){
       alert("Please fill in a number"); 
       accountObject.addExpenses();
     }
-
+    // adding the user input from the prompt into the array
     else {
       this.expensesArray.push({ expenseDescription, expenseAmount });
+    //[{expensDescription: "one", expenseAmount: 1000}, {expensDescription: "two", expenseAmount: 2000}]
+    // {object} within curly braces: an object
     }
   
+    // going back to the menu
     menu(); 
+    
   },
 
    
-     
-  
-
-    //[{expensDescription: "one", expenseAmount: 1000}, {expensDescription: "two", expenseAmount: 2000}]
-    // {object} within curly braces: an object
-
-/*
-    do {
-      expenseAmount = +alert("Please put in a number");
-    } while(expenseAmount === NaN);
-
-
-    addExpenses();
-*/
-
-// going back to menu
-  
-  
+  // FUNCTIONS
 
 
   addIncome: function () {
-     // this is a function declaration as a property, switch the keyword function the name
-    //addIncome.push(2)
-
-    // so we want to add something from the prompt
-    //const expens = parseFloat(prompt("Please fill in your expens here"));
+    
     const incomeDescription = prompt("Please describe your source of income");
 
     const incomeAmount = parseFloat(prompt("Please add the amount"));
-
-    //[{expensDescription: "one", expenseAmount: 1000}, {expensDescription: "two", expenseAmount: 2000}]
 
     if (isNaN(incomeAmount)){
       alert("Please fill in a number"); 
@@ -80,20 +54,12 @@ const accountObject = {
       this.incomeArray.push({ incomeDescription, incomeAmount });
     }
   
-    // check that our function works
-    //console.log(this.incomeArray);
-
-    // going back to menu
     menu();
   },
-  //empty function
-  // will add an income to the income array
-
+  
+// here we want to list and show all the items in the array
   listAllExpenses: function () {
-    // how do we list all elements in an array?
-    // we always have to loop through to see all elements
-    // if we didnt have an object and just plain variables for loop would work fine
-    // but if we have an object a forEach loop is a better approach
+    // for an object, a forEach loop is better
     let message = "";
     this.expensesArray.forEach(function (expense) {
       message +=
@@ -103,17 +69,13 @@ const accountObject = {
         expense.expenseDescription +
         "\n";
     });
-    // put the alert cause we only want to show it once
-    // but we want to contain all of your expenses
+
     alert(message);
     menu();
   },
 
   listAllIncomes: function () {
-     // how do we list all elements in an array?
-     // we always have to loop through to see all elements
-     // if we didnt have an object and just plain variables for loop would work fine
-     // but if we have an object a forEach loop is a better approach
+  
      let message = "";
      this.incomeArray.forEach(function (income) {
        message +=
@@ -123,43 +85,30 @@ const accountObject = {
         income.incomeDescription +
          "\n";
      });
-     // put the alert cause we only want to show it once
-     // but we want to contain all of your expenses
+  
      alert(message);
      menu();
    },
 
-  //empty function
+ 
+  
   getSummary: function () {
-    // 1. remove income and expense parameter not nessecary
-    // 2. create a variabel to store the total amount of all your incomes and do the same for expenses
-    // 3. loop through expense array with forEach and only grab the expenseAmount property
-    // 4. calculate total sum of each expense amount
-    // 5. do the same for income
-    // 6. calculate the totalIncomes - totalExpenses to get the result
-
+    //I first created variables to store the totals in
     let totalExpenses = 0;
     let totalIncome = 0;
-    //let totalBalance= 0;
     
-
-    // expenses
+  
+    // adding upp the expenses from the expense array with ForEach method
     this.expensesArray.forEach(function (expense) {
       totalExpenses = totalExpenses + expense.expenseAmount;
-      // we only the value of the amount property in the object
-      // [{"food", 1000}, {"rent", 5000}, {"gas", 2000}]
     });
-  //alert(`Your total expenses are ${totalExpenses} `);
 
+     // doing the same with the userinput in the income array
     this.incomeArray.forEach(function (income) {
       totalIncome = totalIncome + income.incomeAmount;
-      alert(`Your total income is ${totalIncome},- and your total total expenses are ${totalExpenses},- \nYour current balance is: ${totalIncome - totalExpenses},- \nHave a nice day!`);
-    
-      // we only have the value of the amount property in the object
-      // [{"food", 1000}, {"rent", 5000}, {"gas", 2000}]
-   
-    //console.log(totalIncome - totalExpenses);
-    //alert(`Your current balance is: \n  ${totalIncome - totalExpenses},- \n Have a nice day!`);
+
+      // and then a template literal in an alert with the adding up/taking the expenses from the income amount
+      alert(`Your total income is ${totalIncome},- and your total total expenses are ${totalExpenses},- \nYour current balance is ${totalIncome - totalExpenses},- \nHave a nice day!`);
     });
 
     menu();
@@ -167,24 +116,10 @@ const accountObject = {
 };
    
 
-
-
-// FUNCTION
-
-// why parseFloat? because it turns/stores the prompt input back as a decimal number
-
-/*accountObject.firstName = prompt("Hello there, what's your name?");
-if (accountObject.firstName === "") {
-  alert("Please put in a name");
-}
-
-else if (accountObject.firstName === Number) {
-  alert("Please put in a name");
-}
-
-menu();
-*/
-
+// MENU
+// I started with a prompt saying 'hello' and craeted the menu function
+// Why parseFloat? because it turns/stores the prompt input back as a decimal number
+// Why did I use if else statements?  If else seems to be the easiest way.
 
 function menu() {
   const choice = parseFloat(
@@ -214,7 +149,7 @@ function menu() {
   else if (choice === 5) {
      accountObject.listAllIncomes();
    }
-
+  // the user must make a choice in the form of a number between 1-5
   else {(alert("Please choose a number between 1-5"));
   menu();
   }
